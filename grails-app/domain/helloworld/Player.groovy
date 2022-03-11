897a2349e00f8cc54de78a896e5cfed11cc69164
Dercio Pale <31033591+derciopale@users.noreply.github.com>
2022-03-11 11:52:04 +0200

    Definido variaveis e relacionamento

---
+++ grails-app/domain/helloworld/Player.groovy
@@ -0,0 +1,11 @@
+package helloworld
+static belongsTo = [team: Team]
+String name
+Integer age
+String position
+
+class Player {
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/helloworld/Team.groovy
@@ -0,0 +1,24 @@
+package helloworld
+        
+        String name
+        String city
+        String stadium
+        String logo
+        String manager
+        static hasMany = [players: Player]
+
+class Team {
+
+    static constraints = {
+        
+    name size: 5..40, blank: false, unique: true
+    city size: 5..30, blank: false
+    stadium size: 5..30, blank: false
+    logo size: 5..255, blank: false
+    manager size: 5..30, blank: false
+    }
+
+    String toString() {
+    name
+}
+}
