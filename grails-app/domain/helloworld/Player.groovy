8e58ad358fc64ad0a335cd7c6577080d957a5b87
Dercio Pale <31033591+derciopale@users.noreply.github.com>
2022-03-11 11:55:28 +0200

    Gerando Controller Player e Team

--- grails-app/domain/helloworld/Player.groovy
+++ grails-app/domain/helloworld/Player.groovy
@@ -7,5 +7,12 @@ String position
 class Player {
 
     static constraints = {
+        name size: 5..40, blank: false
+        age min: 16
+        position size: 5..40, blank: false
+    }
+
+    String toString() {
+    name
     }
 }
